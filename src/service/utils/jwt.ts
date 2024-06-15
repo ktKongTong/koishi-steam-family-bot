d94a18f7b86911e72a7f74ec5a79db271d9934d6
ktKongTong <44502608+ktKongTong@users.noreply.github.com>
2024-06-15 21:32:48 +0800

    :bookmark: 0.0.5

--- src/service/utils/jwt.ts
+++ src/service/utils/jwt.ts
@@ -5,3 +5,7 @@ type JwtPayload = _JwtPayload
 export function jwtDecode(token: string): JwtPayload {
   return _jwtDecode(token)
 }
+
+// export function __jwtDecode(token:string): JwtPayload {
+//   return JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString())
+// }
