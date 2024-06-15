663878ef752e2b51f630e5b5e03c6b4bb55191d8
ktKongTong <44502608+ktKongTong@users.noreply.github.com>
2024-06-15 23:06:22 +0800

    :bookmark: 0.0.6-dev.01

--- src/service/render/components/(chart)/canvas.js
+++ src/service/render/components/(chart)/canvas.js
@@ -1,17 +1,17 @@
-// import {Canvas} from "skia-canvas";
-import { createCanvas as _createCanvas } from 'canvas'
+import { Canvas } from 'skia-canvas'
+// import { createCanvas as _createCanvas } from 'canvas'
 // import { createCanvas as _createCanvas } from '@napi-rs/canvas'
 
 export const createCanvas = (widht, height) => {
   // node-canvas
-  return _createCanvas(widht, height)
+  // return _createCanvas(widht, height)
   // skia
-  // return new Canvas(widht, height)
+  return new Canvas(widht, height)
 }
 
 export const canvasToDataURL = (canvas) => {
   // skia
-  // return  canvas.toDataURLSync('png')
+  return canvas.toDataURLSync('png')
   // node / canvas
-  return canvas.toDataURL('image/png')
+  // return canvas.toDataURL('image/png')
 }
