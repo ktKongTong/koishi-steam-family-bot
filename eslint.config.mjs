5d70a4f62fc298e7dfe3ad761529f7292d811ac3
ktKongTong <44502608+ktKongTong@users.noreply.github.com>
2024-06-23 01:09:06 +0800

    add cf-discord impl

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,7 +19,8 @@ export default [
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/ban-ts-comment": "off",
-      "@typescript-eslint/no-require-imports":"off"
+      "@typescript-eslint/no-require-imports":"off",
+      "@typescript-eslint/no-unnecessary-type-constraint":"off"
     }
   },
 ];
